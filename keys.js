b8c4fc80c8b11e3fac2e0e2ce9290c30293ed24a
Kristin Glaze <ikglaze33@gmail.com>
2017-08-27 18:57:06 -0400

    Completed step 6.

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'wC6YWYK57XnhQKm12oySueftR',
+  consumer_secret: 'zEr6C9z5IE5XWeSa9vMIyM7Cr4PAMg59l2Hb53JpZTQmFrnN5p',
+  access_token_key: '901917839870844932-84PSgWHens3RHGqnPxN1SKzrPFOVWdl',
+  access_token_secret: 'qEXYmtjmgkmpvuFMsufKD1vGXhnAnTwMGaIdiCfTxbFiY',
+}
