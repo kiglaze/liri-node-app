1bacca3f64729a1d208fb08ea9381b39e558a627
Kristin Glaze <ikglaze33@gmail.com>
2017-08-28 06:29:43 -0400

    Added Spotify API keys.

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '901917839870844932-84PSgWHens3RHGqnPxN1SKzrPFOVWdl',
   access_token_secret: 'qEXYmtjmgkmpvuFMsufKD1vGXhnAnTwMGaIdiCfTxbFiY',
 }
+
+exports.spotifyKeys = {
+	client_id: 'e6474b87fed64cd39db3c622434d7f94',
+	client_secret: '864d099006d1490784b0f153e5d0c4c9'
+}
