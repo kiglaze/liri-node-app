81a23cb4cbb439e41bb9a225bcd95a7850d747c9
Kristin Glaze <ikglaze33@gmail.com>
2017-08-28 06:31:50 -0400

    Displays all Spotify information when the spotify command is used. Also added the created at information to the twitter tweet results that get displayed when the twitter command is used.

--- liri.js
+++ liri.js
@@ -5,12 +5,21 @@ var Twitter = require('twitter');
 var twitterParams = {screen_name: 'KIGDevAccount'};
 var twitterClient = new Twitter(myKeys.twitterKeys);
 
+var Spotify = require('node-spotify-api');
+var spotifyKeys = myKeys.spotifyKeys;
+var spotifyClient = new Spotify({
+	id: spotifyKeys.client_id,
+	secret: spotifyKeys.client_secret
+});
+
 
 switch(command) {
 	case("my-tweets"):
 		printTwitterTweets(twitterParams, twitterClient);
 		break;
-	case("asdf"):
+	case("spotify-this-song"):
+		var songQuery = (process.argv[3]) ? (process.argv[3]) : "The Sign";
+		searchSpotify(spotifyClient, songQuery);
 		break;
 	case("qwerty"):
 		break;
@@ -19,8 +28,11 @@ switch(command) {
 function printTwitterTweets(twitterParams, twitterClient) {
 	twitterClient.get('statuses/user_timeline', twitterParams, function(error, tweets, response) {
 	  if (!error) {
+	  	console.log("------------------");
 	    for (var i = 0; i < tweets.length; i++) {
 	    	console.log(tweets[i].text);
+	    	console.log(tweets[i].created_at);
+	    	console.log("------------------");
 	    }
 	  } else {
 	  	console.log("Error...");
@@ -29,4 +41,30 @@ function printTwitterTweets(twitterParams, twitterClient) {
 	});
 }
 
-
+function searchSpotify(spotifyClient, query) {
+	spotifyClient.search({ type: 'track', query: query }, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+		var tracksItems = data.tracks.items;
+		console.log("------------------");
+		for (var i = tracksItems.length - 1; i >= 0; i--) {
+			var trackItem = tracksItems[i];
+			console.log("Name: ");
+			console.log(trackItem.name);
+			var artists = trackItem.artists;
+			console.log("...");
+			console.log("Artists: ");
+			for (var j = artists.length - 1; j >= 0; j--) {
+				console.log(artists[j].name);
+			}
+			console.log("...");
+			console.log("Preview URL: ");
+			console.log(trackItem.preview_url);
+			console.log("...");
+			console.log("Album: ");
+			console.log(trackItem.album.name);
+			console.log("------------------");
+		}
+	});
+}
